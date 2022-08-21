d55a1a79da7534ed8d519d7a792e9dfef2389b33
leonardo <leosantana.silva27@gmail.com>
2022-08-21 15:35:56 -0300

    refeito

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,6 @@
 import style from '../styles/Notes.module.css'
 import { useState } from 'react'
-import { IoIosAdd, IoIosCheckmarkCircleOutline} from 'react-icons/io'
-import { BsFillPencilFill, BsFillTrashFill } from 'react-icons/Bs'
+
 export default function App() {
     const [value, setValue] = useState('')
     const [item, setItem] = useState('')
@@ -22,20 +21,13 @@ export default function App() {
           <div className={style.text} id="text">{value}</div>
           <div className={style.buttons}>
           <button className={style.button}>
-            <BsFillPencilFill style={{
-              color: '#fff'
-            }}/>
+            
           </button>
           <button className={style.button} onClick={() => check()}>
-            <IoIosCheckmarkCircleOutline style={{
-              color: '#fff',
-              fontSize: '1.3rem',
-            }} />
+            
           </button>
           <button className={style.button} onClick={() => remove()}>
-            <BsFillTrashFill style={{
-              color: '#fff',
-            }}/>
+            
           </button>
           </div>
         </div>)
@@ -48,10 +40,7 @@ export default function App() {
           <div className={style.add}>
             <input type="text" className={style.display} value={value} onInput={(e) => setValue(e.target.value)}/>
             <button className={style.addButton} onClick={() => createItem()}>
-                <IoIosAdd style={{
-                  color: '#fff',
-                  fontSize: '1.6rem'
-                }}/>
+                
             </button>
           </div>
           <div className={style.notes} id="notes">
